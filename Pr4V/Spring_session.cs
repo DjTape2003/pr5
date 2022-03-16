c56621f74928ba346718c4f47af513a2150ec87f
Байкалов Владислав Евгеньевич гр. ИС(ПРО)-31 <is12750@stud.KHPK.RU>
2022-03-16 12:02:05 +0700

    pr5

---
+++ Pr4V/Spring_session.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pr4V
+{
+    public class Spring_session:Session
+    {
+        
+        protected int Practic_grade;
+        protected String Practic_name;
+
+        public string _Practic_name
+        {
+            get { return Practic_name; }
+            set { Practic_name = value; }
+        }
+
+
+        public int _Practic_grade
+        {
+            get { return Practic_grade; }
+            set {
+                if (value >= 1 && value >= 5)
+                {
+                    Practic_grade = value;
+                }
+            }
+        }
+
+        
+        public override void PrintSession()
+        {
+            Console.WriteLine("Семестр = " + Semester + " |" +
+               " Зачётов = " + Test + " |" +
+               " Екзаменов = " + Exam + " |"+
+               " Практика = " + Practic_name + " |"+
+               " Оценка = " + Practic_grade + " |"
+               );
+        }
+    }
+}
